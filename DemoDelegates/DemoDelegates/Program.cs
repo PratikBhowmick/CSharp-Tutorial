4a606ced921383591bd713614dfe0137af5c4d51
Pratik <pratik.bhowmick@yahoo.co.in>
2019-05-18 11:47:14 +0530

    Demo delegates

---
+++ DemoDelegates/DemoDelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDelegates
+{
+    delegate int NumberChanger(int n);
+    class Program
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            nc1 = nc1 + nc2;
+
+            //calling the methods using the delegate objects
+            nc1(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            //nc2(5);
+            //Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
